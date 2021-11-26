0f9b1059954133c62e92d26a5162a7c927a8510a
Yaroslav Zhvalevskiy <yaroslav.zhvalevskiy@gmail.com>
2021-11-26 15:30:05 +0300

    changes by feedback

--- src/tests/specs/test.js
+++ src/tests/specs/test.js
@@ -1,36 +1,37 @@
 const ui = require('../../app/ui');
 
-describe('desc', () => {
-    it('test', () => {
+describe('Testing the official WebdriverIO website', () => {
+
+    beforeEach(function () {
         ui.fullScreen();
         ui.url('');
+    });
+    it('Test logos', () => {
         expect(ui.homePage.isDisplayed('mainLogo')).toBeTrue();
         expect(ui.header.isDisplayed('logo')).toBeTrue();
+    });
+    it('Test header', () => {
         ui.header.clickBy('menuItems', 'Docs');
-        ui.pause(2000);
         expect(ui.contentBoard.firstLevelHeader).toHaveText('Getting Started');
+        ui.header.clickBy('menuItems', 2);
+        expect(ui.contentBoard.firstLevelHeader).toHaveText('Introduction');
+    });
+    it('Test sidebar', () => {
+        ui.header.clickBy('menuItems', 'Docs');
         ui.sidebar.clickByText('sidebarItems', 'Reporter');
-        ui.pause(2000);
         ui.sidebar.clickByText('sidebarSubItems', 'Allure Reporter');
-        ui.pause(2000);
         expect(ui.contentBoard.firstLevelHeader).toHaveText('Allure Reporter');
         ui.header.clickBy('menuItems', 2);
-        ui.pause(2000);
-        expect(ui.contentBoard.firstLevelHeader).toHaveText('Introduction');
         ui.sidebar.clickByPos('sidebarItems', 0);
-        ui.pause(2000);
-        ui.sidebar.clickByPos('sidebarSubItems', 6);
-        ui.pause(2000);
-        expect(ui.contentBoard.firstLevelHeader).toHaveText('Firefox');
+        ui.sidebar.clickByPos('sidebarSubItems', 2);
+        expect(ui.contentBoard.firstLevelHeader).toHaveText('WebDriver Protocol');
+    });
+    it('Test footer', () => {
         ui.footer.scrollDown('footerItem');
-        ui.pause(2000);
-        ui.footer.clickByText('footerItemsWithInternalLinks', 'Help')
-        ui.pause(2000);
+        ui.footer.clickByText('footerItemsWithInternalLinks', 'Help');
         expect(ui.contentBoard.firstLevelHeader).toHaveText('Need Help?');
         ui.footer.scrollDown('footerItem');
-        ui.pause(2000);
         ui.footer.clickByText('footerItemsWithExternalLinks', 'Donate to WebdriverIO');
-        ui.pause(5000);
         expect(ui.getUrl() === 'https://opencollective.com/webdriverio');
     });
 });
